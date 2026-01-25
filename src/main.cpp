2f7712c2882c64203a1895d3e1d9090fb7773d02
Стефан Монов <logixoul@gmail.com>
2026-01-25 22:29:38 +0200

    sph now works

--- src/main.cpp
+++ src/main.cpp
@@ -26,115 +26,19 @@ struct Particle {
 	vec2 pos;
 	vec2 velocity;
 	vec2 force;
-	float rho, p; // density and pressure
+	float densityHere;
+	float pressureHere;
+	vec2 velocityContribs;
+	float velocityContribsSumWeights;
 };
 
 
 
-const static vec2 G(0.f, 10.f);   // external (gravitational) forces
-const static float REST_DENS = 300.f;  // rest density
-const static float GAS_CONST = 2000.f; // const for equation of state
-const static float H = 16.f;           // kernel radius
-const static float HSQ = H * H;        // radius^2 for optimization
-const static float MASS = 2.5f;        // assume all particles have the same mass
-const static float VISC = 200.f;       // viscosity constant
-const static float DT = 0.0007f;       // integration timestep
-
-// smoothing kernels defined in Müller and their gradients
-// adapted to 2D per "SPH Based Shallow Water Simulation" by Solenthaler et al.
-const static float POLY6 = 4.f / (M_PI * pow(H, 8.f));
-const static float SPIKY_GRAD = -10.f / (M_PI * pow(H, 5.f));
-const static float VISC_LAP = 40.f / (M_PI * pow(H, 5.f));
-
-// simulation parameters
-const static float EPS = H; // boundary epsilon
-const static float BOUND_DAMPING = -0.5f;
 
 vector<Particle> particles;
 
 void reset() {
 	particles.clear();
-}
-
-void ComputeDensityPressure(void)
-{
-	for (auto& pi : particles)
-	{
-		pi.rho = 0.f;
-		for (auto& pj : particles)
-		{
-			vec2 rij = pj.pos - pi.pos;
-			float r2 = dot(rij, rij);
-
-			if (r2 < HSQ)
-			{
-				// this computation is symmetric
-				pi.rho += MASS * POLY6 * pow(HSQ - r2, 3.f);
-			}
-		}
-		pi.p = GAS_CONST * (pi.rho - REST_DENS);
-	}
-}
-	
-void ComputeForces(void)
-{
-	for (auto& pi : particles)
-	{
-		vec2 fpress(0.f, 0.f);
-		vec2 fvisc(0.f, 0.f);
-		for (auto& pj : particles)
-		{
-			if (&pi == &pj)
-			{
-				continue;
-			}
-
-			vec2 rij = pj.pos - pi.pos;
-			float r = length(rij);
-
-			if (r < H)
-			{
-				// compute pressure force contribution
-				fpress += -normalize(rij) * MASS * (pi.p + pj.p) / (2.f * pj.rho) * SPIKY_GRAD * pow(H - r, 3.f);
-				// compute viscosity force contribution
-				fvisc += VISC * MASS * (pj.velocity - pi.velocity) / pj.rho * VISC_LAP * (H - r);
-			}
-		}
-		vec2 fgrav = G * MASS / pi.rho;
-		pi.force = fpress + fvisc + fgrav;
-	}
-}
-
-void Integrate(void)
-{
-	for (auto& p : particles)
-	{
-		// forward Euler integration
-		p.velocity += DT * p.force / p.rho;
-		p.pos += DT * p.velocity;
-
-		// enforce boundary conditions
-		if (p.pos.x - EPS < 0.f)
-		{
-			p.velocity.x *= BOUND_DAMPING;
-			p.pos.x = EPS;
-		}
-		if (p.pos.x + EPS > sx)
-		{
-			p.velocity.x *= BOUND_DAMPING;
-			p.pos.x = sx - EPS;
-		}
-		if (p.pos.y - EPS < 0.f)
-		{
-			p.velocity.y *= BOUND_DAMPING;
-			p.pos.y = EPS;
-		}
-		if (p.pos.y + EPS > sy)
-		{
-			p.velocity.y *= BOUND_DAMPING;
-			p.pos.y = sy - EPS;
-		}
-	}
 }
 
 bool pause = false;
@@ -205,14 +109,24 @@ struct SApp : ci::app::App {
 		direction = vec2(getMousePos()) - lastm;
 		lastm = getMousePos();
 	}
+	float surfaceTensionThreshold = 1.0f;
+	float pushawayCoef = .2f;
+	float surfaceTensionCoef = 0.2f;
 	void stefanDraw()
 	{
 		static float blurSize = 1.41f;
 		ImGui::DragFloat("blurSize", &blurSize, 1.0f, 0.1, 100, "%.3f", ImGuiSliderFlags_Logarithmic);
-		static int blurIters = 5;
+		static int blurIters = 3;
 		ImGui::DragInt("blurIters", &blurIters, 1.0f, 1, 16, "%d", ImGuiSliderFlags_None);
 		static float gradScale = 1.68;
 		ImGui::DragFloat("gradScale", &gradScale, 1.0f, 1, 2000, "%.3f", ImGuiSliderFlags_Logarithmic);
+		static float renderThreshold = 0.07f;
+		ImGui::DragFloat("renderThreshold", &renderThreshold, 1.0f, .001f, 20, "%.3f", ImGuiSliderFlags_Logarithmic);
+
+		ImGui::DragFloat("surfaceTensionThreshold", &surfaceTensionThreshold, .1f, .001f, 20.0f, "%.3f", ImGuiSliderFlags_Logarithmic);
+		ImGui::DragFloat("pushawayCoef", &pushawayCoef, .1f, .001f, 20.0f, "%.3f", ImGuiSliderFlags_Logarithmic);
+		ImGui::DragFloat("surfaceTensionCoef", &surfaceTensionCoef, .1f, .001f, 20.0f, "%.3f", ImGuiSliderFlags_Logarithmic);
+
 		static float bloomSize = 1.5f;
 		static int bloomIters = 6.0f;
 		static float bloomIntensity = 0.07f;
@@ -229,12 +143,20 @@ struct SApp : ci::app::App {
 		}
 		
 		auto tex = gtex(img);
-		tex = gpuBlur2_5::run_longtail(tex, blurIters, blurSize);
-		auto thresholdedTex = shade2(tex,
+		auto texB = gpuBlur2_5::run(tex, blurIters);
+		auto thresholdedTex = shade2(texB,
 			"float f = fetch1();"
 			"float fw = fwidth(f);"
-			"f = smoothstep(0.5-fw/2, 0.5+fw/2, f);"
+			"f = smoothstep(0.15-fw/2, 0.15+fw/2, f);"
 			"_out.r = f;");
+
+		auto limitedTex = shade2(texB,
+			"float f = fetch1();"
+			"float fw = fwidth(f);"
+			"f *= smoothstep(renderThreshold-fw/2, renderThreshold+fw/2, f);"
+			"_out.r = f;",
+			ShadeOpts().uniform("renderThreshold", renderThreshold)
+		);
 
 		auto redTex = thresholdedTex;
 		
@@ -253,9 +175,9 @@ struct SApp : ci::app::App {
 		static auto envMap = gl::Texture::create(ci::loadImage(loadAsset("envmap2.png")));
 		//static auto envMap = gl::TextureCubeMap::create(loadImage(loadAsset("envmap_cube.jpg")), gl::TextureCubeMap::Format().mipmap());
 		//gl::TextureBaseRef test=envMap;
-		auto grads = get_gradients_tex(tex);
+		auto grads = get_gradients_tex(limitedTex);
 
-		auto tex2 = shade2(tex, grads, envMap, redTex, /*greenTex,*/
+		auto tex2 = shade2(texB, grads, envMap, redTex, /*greenTex,*/
 			"vec2 grad = fetch2(tex2) * gradScale;"
 			"vec3 N = normalize(vec3(-grad.x, -grad.y, -1.0));"
 			"vec3 I=-normalize(vec3(tc.x-.5, tc.y-.5, 1.0));"
@@ -267,8 +189,8 @@ struct SApp : ci::app::App {
 			"vec3 albedo = 0.0*vec3(0.005, 0.0, 0.0);"
 			//"c = mix(albedo, c, pow(.9, fetch1(tex) * 50.0));" // tmp
 			"R = reflect(I, N);"
-			//"if(fetch1(tex) > surfTensionThres)"
-			//"	c += getEnv(R)*0.2;" // mul to tmp simulate one side of the envmap being brighter than the other
+			"if(fetch1(tex) > surfTensionThres)"
+			"	c += getEnv(R)*0.2;"
 
 			"float redVal = fetch1(tex4);"
 			//"redVal/=redVal+1.0;" // reinhard
@@ -282,19 +204,12 @@ struct SApp : ci::app::App {
 			"_out.rgb = c;"
 			, ShadeOpts().ifmt(GL_RGB16F).scale(4.0f).uniform("surfTensionThres", 0.1f).uniform("gradScale", gradScale),
 			"float PI = 3.14159265358979323846264;\n"
-			"vec2 latlong(vec3 v) {\n"
-			"v = v.xzy;\n"
-			"v = normalize(v);\n"
-			"float theta = asin(v.z);\n" // +z is up
-			"\n"
-			"v.y=-v.y;\n"
-			"float phi = atan(v.y, v.x) + PI/2;\n"
-			"return vec2(phi / (PI), theta / (PI/2.0));\n"
+			"vec2 latlong(vec3 refl) {\n"
+			"	return vec2(atan(refl.z, refl.x) / (2.0 * PI) + 0.5, asin(clamp(refl.y, -1.0, 1.0)) / PI + 0.5);"
 			"}\n"
-			"vec3 w = vec3(.22, .71, .07);"
 			"vec3 getEnv(vec3 v) {\n"
 			"	vec3 c = fetch3(tex3, latlong(v));\n"
-			"	c = pow(c, vec3(2.2));" // gamma correction
+			//"	c = pow(c, vec3(2.2));" // gamma correction
 			"	return c;"
 			"}\n"
 		);
@@ -318,6 +233,12 @@ struct SApp : ci::app::App {
 		//videoWriter->write(tex2);
 		
 		gl::draw(tex2, getWindowBounds());
+		
+		/*gl::setMatricesWindow(getWindowSize(), true);
+		for (auto& p : particles) {
+			gl::color(Color::white());
+			gl::drawSolidCircle(p.pos * float(::scale), 4);
+		}*/
 	}
 	void stefanUpdate()
 	{
@@ -337,7 +258,7 @@ struct SApp : ci::app::App {
 		else if (mouseDown_[2]) {
 			for (Particle& part : particles) {
 				if (distance(part.pos, scaledm) < 40) {
-					const float velocityScaleFactor = 2000.0f / (float)::scale;
+					const float velocityScaleFactor = 0.6f / (float)::scale;
 					part.velocity += velocityScaleFactor * direction;
 					float speed = glm::length(part.velocity);
 					float newSpeed = std::min(speed, velocityScaleFactor * 10);
@@ -347,10 +268,109 @@ struct SApp : ci::app::App {
 		}
 	}
 
+	float steepKernel(float dist) {
+		if (dist >= MAX_DIST) return 0.0f;
+		float x = 1.0f - dist / MAX_DIST;
+		return x * x;
+	}
+
+	const float MAX_DIST = 20;
+	void forEachNeighbourPair(std::function<void(Particle&, Particle&, vec2 const&, float)> const& cb) {
+		for (int i = 0; i < particles.size(); i++) {
+			auto& p1 = particles[i];
+			for (int j = i + 1; j < particles.size(); j++) {
+				auto& p2 = particles[j];
+				auto vec = p1.pos - p2.pos;
+				float dist = length(vec);
+				if (dist <= MAX_DIST) {
+					cb(p1, p2, vec, dist);
+				}
+			}
+		}
+	}
+
+	void lxComputeForces() {
+		for (auto& p : particles) {
+			p.densityHere = 0;
+			p.pressureHere = 0;
+		}
+		
+		forEachNeighbourPair([&](auto& p1, auto& p2, vec2 const& vec, float dist) {
+			auto vecNorm = vec / dist; // normalized
+			float f = steepKernel(dist);
+			
+			float densityToAdd = f;
+			p1.densityHere += densityToAdd;
+			p2.densityHere += densityToAdd;
+		});
+
+		for (auto& p : particles) {
+			p.pressureHere = p.densityHere - surfaceTensionThreshold;
+		}
+
+		forEachNeighbourPair([&](auto& p1, auto& p2, vec2 const& vec, float dist) {
+			auto vecNorm = vec / (dist + 0.0001f); // normalized
+			float f = steepKernel(dist);
+			vec2 pushawayVec = vecNorm * f;
+			const float pressureSum = p1.pressureHere + p2.pressureHere;
+			p1.force += pushawayVec * surfaceTensionCoef * pressureSum;
+			p2.force -= pushawayVec * surfaceTensionCoef * pressureSum;
+		});
+
+		smoothenVelocities();
+	}
+
+	void smoothenVelocities() {
+		for (auto& p : particles) {
+			float f = steepKernel(0.0f);
+			p.velocityContribs = p.velocity * f;
+			p.velocityContribsSumWeights = f;
+		}
+		forEachNeighbourPair([&](Particle& p1, Particle& p2, vec2 const& vec, float dist) {
+			float f = steepKernel(dist);
+			p1.velocityContribs += p2.velocity * f;
+			p2.velocityContribs += p1.velocity * f;
+			p1.velocityContribsSumWeights += f;
+			p2.velocityContribsSumWeights += f;
+			});
+		for (auto& p : particles) {
+			p.velocity = p.velocityContribs / p.velocityContribsSumWeights;
+		}
+	}
+
+
 	void doFluidStep() {
-		ComputeDensityPressure();
-		ComputeForces();
-		Integrate();
+		const auto maxX = sx;
+		const auto maxY = sy;
+		lxComputeForces();
+		const float dampening = 0.6;
+		for (auto& p : particles) {
+			if (p.pos.x < 0 || p.pos.x > maxX) {
+				p.velocity.x *= -1;
+				p.velocity.x *= dampening;
+			}
+			if (p.pos.y < 0 || p.pos.y > maxY) {
+				p.velocity.y *= -1;
+				p.velocity.y *= dampening;
+			}
+			if (p.pos.x < 0) {
+				p.pos.x = -p.pos.x;
+			}
+			if (p.pos.x > maxX) {
+				p.pos.x = maxX - (p.pos.x - maxX);
+			}
+			if (p.pos.y < 0) {
+				p.pos.y = -p.pos.y;
+			}
+			if (p.pos.y > maxY) {
+				p.pos.y = maxY - (p.pos.y - maxY);
+			}
+		}
+		for (auto& p : particles) {
+			p.velocity += p.force + vec2(0, 0.07);
+			p.pos += p.velocity;
+			p.force = vec2(0, 0);
+		}
 	}
 };
 
